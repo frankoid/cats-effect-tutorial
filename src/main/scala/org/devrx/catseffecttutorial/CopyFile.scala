bafb825b8fc833bfb4f4dc73f3ae325d17a97c06
Francis Devereux <francis@devrx.org>
2022-01-31 22:17:11 +0000

    Up to "Acquiring and releasing Resources"

---
+++ src/main/scala/org/devrx/catseffecttutorial/CopyFile.scala
@@ -0,0 +1,32 @@
+package org.devrx.catseffecttutorial
+
+import cats.effect.IO
+import cats.effect.kernel.Resource
+
+import java.io.*
+
+object CopyFile {
+
+  def copy(origin: File, destination: File): IO[Long] = ???
+
+  def inputStream(f: File): Resource[IO, FileInputStream] =
+    Resource.make {
+      IO.blocking(new FileInputStream(f))
+    } { inStream =>
+      IO.blocking(inStream.close()).handleErrorWith(_ => IO.unit)
+    }
+
+  def outputStream(f: File): Resource[IO, FileOutputStream] =
+    Resource.make {
+      IO.blocking(new FileOutputStream(f))
+    } { outStream =>
+      IO.blocking(outStream.close()).handleErrorWith(_ => IO.unit)
+    }
+
+  def inputOutputStreams(in: File, out: File): Resource[IO, (InputStream, OutputStream)] =
+    for {
+      inStream <- inputStream(in)
+      outStream <- outputStream(out)
+    } yield (inStream, outStream)
+
+}
