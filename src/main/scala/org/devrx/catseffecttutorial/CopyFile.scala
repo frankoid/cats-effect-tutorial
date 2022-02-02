458594ac4e2dbbf5e1fff3b9f44a276ec41951d6
Francis Devereux <francis@devrx.org>
2022-02-02 20:52:04 +0000

    https://typelevel.org/cats-effect/docs/tutorial#copying-data

--- src/main/scala/org/devrx/catseffecttutorial/CopyFile.scala
+++ src/main/scala/org/devrx/catseffecttutorial/CopyFile.scala
@@ -7,7 +7,21 @@ import java.io.*
 
 object CopyFile {
 
-  def copy(origin: File, destination: File): IO[Long] = ???
+  def copy(origin: File, destination: File): IO[Long] =
+    inputOutputStreams(origin, destination).use { case (in, out) =>
+      transfer(in, out)
+    }
+
+  def transfer(origin: InputStream, destination: OutputStream): IO[Long] =
+    transmit(origin, destination, new Array[Byte](1024 * 10), 0L)
+
+  def transmit(origin: InputStream, destination: OutputStream, buffer: Array[Byte], acc: Long): IO[Long] =
+    for {
+      amount <- IO.blocking(origin.read(buffer, 0, buffer.length))
+      count <-
+        if (amount > -1) IO.blocking(destination.write(buffer, 0, amount)) >> transmit(origin, destination, buffer, acc + amount)
+        else IO.pure(acc) // End of read stream reached (by java.io.InputStream contract), nothing to write
+    } yield count // Returns the actual amount of bytes transmitted
 
   def inputStream(f: File): Resource[IO, FileInputStream] =
     Resource.make {
